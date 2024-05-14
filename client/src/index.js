e1cdff2e0a72623831503a5e99cf28678577ac10
Wanying Song <songwanying2015@gmail.com>
2024-05-13 20:02:18 -0400

    Update_1

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+
+const container = document.getElementById('root');
+const root = createRoot(container); // Create a root.
+
+root.render(
+<React.StrictMode>
+    
+    <App />
+    
+</React.StrictMode>
+);
--- server/index.js
+++ server/index.js
@@ -3,26 +3,32 @@ require('dotenv').config();
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-const PORT = 3001;
-const API_KEY = 'YOUR_API_KEY';
+
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.get('/api/search', async (req, res) => {
     const { query } = req.query;
+    const encodedQuery = encodeURIComponent(query); // Encode the query parameter
+    const url = `https://webknox-recipes.p.rapidapi.com/recipes/findByIngredients?ingredients=${encodedQuery}&number=10`;
+    const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+        'X-RapidAPI-Host': 'webknox-recipes.p.rapidapi.com'
+    }
+    };
+
     try {
-    const response = await fetch(`https://webknox-recipes.p.rapidapi.com/recipes/search?query=${query}&number=10`, {
-        headers: {
-        'X-RapidAPI-Key': API_KEY,
-        },
-    });
+    const response = await fetch(url, options);
     if (!response.ok) {
         throw new Error('Failed to fetch recipes');
     }
     const data = await response.json();
     res.json(data);
     } catch (error) {
-    console.error('Error fetching recipes:', error);
+    console.error('Error searching recipes:', error);
     res.status(500).json({ error: 'Failed to fetch recipes' });
     }
 });
